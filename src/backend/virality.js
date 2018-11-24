8d5c0a6b1f538ea088b1f1fd63182e5a70107ef6
Patrick Juen <patrickrainier@gmail.com>
2018-11-25 02:19:14 +0800

    Implement virality computation

---
+++ src/backend/virality.js
@@ -0,0 +1,41 @@
+const bracket = [
+    { min: -Infinity, max: 4, value: 1.0 },
+    { min: 5, max: 9, value: 2.0 },
+    { min: 10, max: 19, value: 3.0 },
+    { min: 20, max: 29, value: 4.0 },
+    { min: 30, max: 44, value: 5.5 },
+    { min: 45, max: 59, value: 7.0 },
+    { min: 60, max: 79, value: 9.0 },
+    { min: 80, max: 99, value: 11.0},
+    { min: 100, max: 129, value: 14.0 },
+    { min: 130, max: 159, value: 17.0},
+    { min: 160, max: 249, value: 21.0},
+    { min: 250, max: 299, value: 26.0},
+    { min: 300, max: 399, value: 30.0},
+    { min: 400, max: 599, value: 40.0},
+    { min: 600, max: Infinity, value: 0.1} // rate multiplier
+]
+
+// [1543070446529, 1543070446529, 1543070446529, 1543070446529, 1543070446529]
+
+const isMaximum = (currentBracket) => currentBracket.max === Infinity
+
+const getMultiplierValue = (currentValue, currentBracket) => {
+    const { min, value } = currentBracket
+    const previousBracket = bracket.slice(-2)[0]
+    // console.log(currentValue, min, value, previousBracket)
+    return ((currentValue - min) * value ) +  previousBracket.value
+}
+
+const computeVirality = (upvotes) => {
+    const now = Date.now()
+    const oneMinuteAgo = now - 60000
+    const twoMinutesAgo = now - 120000
+    const currentRate = upvotes.filter(upvote => oneMinuteAgo <= upvote && now >= upvote).length
+    const previousRate = upvotes.filter(upvote => twoMinutesAgo <= upvote && oneMinuteAgo > upvote).length
+    const acceleration = currentRate - previousRate
+    const currentBracket = bracket.find(entry => entry.min <= acceleration && entry.max >= acceleration )
+    return isMaximum(currentBracket) ? getMultiplierValue(acceleration, currentBracket) : currentBracket.value 
+}
+
+export default computeVirality
---
+++ tests/virality.test.js
@@ -0,0 +1,61 @@
+import { expect } from 'chai'
+import MockDate from 'mockdate'
+import computeVirality from '../src/backend/virality'
+
+const fixtures = [
+    1543079319021,
+    1543079320021,
+    1543085321021,
+    1543089322021,
+    1543089323021,
+    1543089324021,
+]
+
+const generateFixtures = (numElements) => {
+    return Array(numElements).fill(Date.now())
+}
+
+describe('Virality', () => {
+
+    afterEach(() => MockDate.reset())
+
+    it('computes virality', () => {
+        const value = computeVirality(fixtures)
+        expect(value).to.be.equal(1.0)
+        // console.log('result', value)
+    })
+
+    it ('computes virality of viral meme', () => {
+        MockDate.set('10/15/2018 13:00')
+        let timestamps = []
+        timestamps = timestamps.concat(generateFixtures(20))
+        MockDate.set('10/15/2018 13:01')
+        timestamps = timestamps.concat(generateFixtures(30))
+        // MockDate.set('10/15/2018 13:02')
+        const value = computeVirality(timestamps)
+        expect(value).to.be.equal(7.0)
+    })
+
+    it ('computes virality of viral meme', () => {
+        MockDate.set('10/15/2018 13:00')
+        let timestamps = []
+        timestamps = timestamps.concat(generateFixtures(20))
+        MockDate.set('10/15/2018 13:01')
+        timestamps = timestamps.concat(generateFixtures(30))
+        MockDate.set('10/15/2018 13:02')
+        const value = computeVirality(timestamps)
+        expect(value).to.be.equal(3.0)
+    })
+
+    it('computes virality max bracket, 700', () => {
+        const timestamps = generateFixtures(700)
+        const value = computeVirality(timestamps)
+        expect(value).to.be.equal(50.0)
+    })
+
+    it('computes virality max bracket, 1500', () => {
+        const timestamps = generateFixtures(1500)
+        const value = computeVirality(timestamps)
+        expect(value).to.be.equal(130.0)
+    })
+})
