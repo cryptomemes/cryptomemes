57468949da0efb6dae0c6acc3b2015cf9a753587
Tristan <trrstnm@gmail.com>
2018-11-24 19:36:16 +0800

    refactors front-end codebase

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import { Provider } from 'mobx-react';
-import { Route, Switch, HashRouter as Router } from 'react-router-dom';
-import RootStore from './stores/';
-import MemePage from './pages/Memes/';
-import LandingPage from './pages/Landing/';
+import React from 'react'
+import { Provider } from 'mobx-react'
+import { Route, Switch, HashRouter as Router } from 'react-router-dom'
+import RootStore from './stores/'
+import MemesPage from './pages/Memes/'
 
-const store = new RootStore();
-window.store = store;
+const store = new RootStore()
+window.store = store
 
 const App = () => (
-    <Provider {...store}>
-        <Router>
-            <div>
-                <Route path="/" component={LandingPage} />
-                <Route path="/memes" component={MemePage} />
-            </div>
-        </Router>
-    </Provider>
-);
+  <Provider {...store}>
+    <Router>
+      <Route path="/" component={MemesPage} />
+    </Router>
+  </Provider>
+)
 
-export default App;
+export default App
--- src/frontend/pages/Memes/index.jsx
+++ src/frontend/pages/Memes/index.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Card, Row, Col, Icon } from 'antd';
+import { Card, Row, Col, Layout, Menu, Icon } from 'antd';
 import styled from 'styled-components';
 
 const { Meta } = Card;
+const { Header, Footer } = Layout
 
 const sampleMemes = [
   {
@@ -21,14 +22,14 @@ const sampleMemes = [
     title: 'Chang From Hangover',
     owner: 'Jackie Chan',
   },
-];
+]
 
 const MemeContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`;
+`
 
 const CardActionsContainer = styled.div`
   margin-top: 1em;
@@ -36,7 +37,28 @@ const CardActionsContainer = styled.div`
   justify-content: end;
   align-items: end;
   flex-direction: row;
-`;
+`
+const NavBar = () => (
+  <Header className="header">
+    <div className="logo" />
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={['2']}
+      style={{ lineHeight: '64px' }}
+    >
+      <Menu.Item key="1">nav 1</Menu.Item>
+      <Menu.Item key="2">nav 2</Menu.Item>
+      <Menu.Item key="3">nav 3</Menu.Item>
+    </Menu>
+  </Header>
+)
+
+const PageFooter = () => (
+  <Footer style={{ textAlign: 'center' }}>
+    CryptoMemes ©2018 Created by CryptoChamps
+  </Footer>
+)
 
 const MemeCard = ({ imageSrc, title, owner, liked }) => (
   <Row style={{ padding: '2em' }}>
@@ -58,25 +80,31 @@ const MemeCard = ({ imageSrc, title, owner, liked }) => (
       </Card>
     </Col>
   </Row>
-);
+)
 
-class MemesPage extends React.Component {
+class MemesPage extends Component {
   componentDidMount() {
-
   }
 
   render() {
     return (
-      <MemeContainer>
-        {sampleMemes.map(meme => <MemeCard
-            imageSrc={meme.imageSrc}
-            title={meme.title}
-            owner={meme.owner}
-            key={meme.title}
-          />)}
-      </MemeContainer>
+      <Fragment>
+        <NavBar />
+        <MemeContainer>
+          {
+            sampleMemes.map(meme => 
+              <MemeCard
+                imageSrc={meme.imageSrc}
+                title={meme.title}
+                owner={meme.owner}
+                key={meme.title}
+              />) 
+          }
+        </MemeContainer>
+        <PageFooter />
+      </Fragment>
     )
   }
 }
 
-export default inject('memeStore')(observer(MemesPage));
+export default inject('memeStore')(observer(MemesPage))
