27ae6b532be6e762e1923069faac9ff1b1cf49b4
Li Arolf  Rey <liarolfrey@Lis-MacBook-Pro.local>
2018-11-25 08:50:11 +0800

    feat: Frontend fetch memes

--- src/frontend/pages/Memes/index.jsx
+++ src/frontend/pages/Memes/index.jsx
@@ -4,30 +4,10 @@ import { Card, Row, Col, Layout, Menu, Icon } from 'antd';
 import styled from 'styled-components'; import '../../css/antd.css'
 import BuySharesModal from '../../components/BuySharesModal'
 import NavBar from '../../components/NavBar';
+import Loader from '../../components/Loader';
 
 const { Meta } = Card;
 const { Footer } = Layout
-
-const sampleMemes = [
-  {
-    imageSrc: 'https://imgix.ranker.com/user_node_img/50056/1001100544/original/v-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces',
-    title: 'Douche Store',
-    owner: 'Carter Williams',
-    saleLimit: '$50',
-  },
-  {
-    imageSrc: 'https://pics.me.me/is-your-body-from-mcdonalds-because-imlovingit-5262227.png',
-    title: 'Mr Bean',
-    owner: 'Mcadoo',
-    saleLimit: '$12',
-  },
-  {
-    imageSrc: 'https://www.yourtango.com/sites/default/files/styles/header_slider/public/display_list/list.jpg?itok=PceCEIvN',
-    title: 'Chang From Hangover',
-    owner: 'Jackie Chan',
-    saleLimit: '$90',
-  },
-]
 
 const MemeContainer = styled.div`
   display: flex;
@@ -50,7 +30,7 @@ const PageFooter = () => (
   </Footer>
 )
 
-const MemeCard = ({ imageSrc, title, owner, liked, saleLimit, onLikeClick }) => (
+const MemeCard = ({ imageSrc, title, price, liked, saleLimit, onLikeClick }) => (
   <Row style={{ padding: '2em' }}>
     <Col span={18}>
       <Card
@@ -67,7 +47,7 @@ const MemeCard = ({ imageSrc, title, owner, liked, saleLimit, onLikeClick }) => 
       >
         <Meta
           title={title}
-          description={`For sale: ${owner}`}
+          description={`For sale: ${price}`}
         />
       </Card>
     </Col>
@@ -85,19 +65,20 @@ class MemesPage extends Component {
   }
 
   render() {
-    const { authStore: { logout } } = this.props
+    const { authStore: { logout }, memeStore: { memes, isMemeFetching } } = this.props
     return (
       <Fragment>
         <NavBar logout={logout} />
         <MemeContainer>
           {
-            sampleMemes.map(meme => 
+            isMemeFetching ? <Loader /> :
+            memes.map(meme => 
               <MemeCard
-                imageSrc={meme.imageSrc}
+                imageSrc={`https://s3-ap-southeast-1.amazonaws.com/crypto-memes/${meme.photoImage}`}
                 title={meme.title}
-                owner={meme.owner}
                 key={meme.title}
-                saleLimit={meme.saleLimit}
+                price={meme.price}
+                saleLimit={meme.sellables.reduce((acc, val) => { return acc + val}, 0)}
                 onLikeClick={this.handleLikeClick}
               />) 
           }
---
+++ src/utils/sanitizeMemes.js
@@ -0,0 +1,52 @@
+const sanitizeMemes = (memes, memeDetails, web3) => {
+  const FIELDS = [
+    'photoImage',
+    'title',
+    'price',
+    'upvotesIndex',
+    'sellablesIndex',
+    'upvotes',
+    'sellables',
+    'owners'
+  ]
+
+  const sanitizedMemes = memes
+  .map((meme, index) => {
+    const concatedMeme = [...meme, ...memeDetails[index]]
+    return concatedMeme 
+  }).map((meme, index) => {
+    const memeObject = {};
+    FIELDS.forEach((field, index) => {
+      switch (field) {
+        case 'photoImage':
+          memeObject[field] = web3.utils.toUtf8(meme[index])
+          break;
+        case 'title':
+          memeObject[field] = web3.utils.toUtf8(meme[index])
+          break;
+        case 'price':
+          memeObject[field] = Number(meme[index])
+          break;
+        case 'upvotesIndex':
+          memeObject[field] = Number(meme[index])
+          break;
+        case 'sellablesIndex':
+          memeObject[field] = Number(meme[index])
+          break;
+        case 'upvotes':
+          memeObject[field] = meme[index] ? meme[index].map(a => Number(a)) : []
+          break;
+        case 'sellables':
+          memeObject[field] = meme[index] ? meme[index].map(a => Number(a)) : []
+          break;
+        default:
+          memeObject[field] = meme[index]
+          break;
+      }
+    })
+    return memeObject
+  })
+  return sanitizedMemes
+}
+
+export default sanitizeMemes
