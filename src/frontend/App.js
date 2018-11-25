53d851a1d48b7e3125b14b5e1b5367af82f5c6c2
Brent <brent.tudas@gmail.com>
2018-11-25 11:05:04 +0800

    feat(Header Routes and App Router Structure):

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { Provider } from 'mobx-react'
-import { Route, HashRouter as Router } from 'react-router-dom'
+import { Route, Switch, HashRouter as Router } from 'react-router-dom'
 import RootStore from './stores/'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
@@ -14,14 +14,14 @@ window.store = store
 const App = () => (
   <Provider {...store}>
     <Router>
-      <Fragment>
+      <Switch>
         <Route exact path="/" component={Main} />
         <Fragment>
           <NavBar />
-          <Route path="/profile" component={Profile} />
+          <Route exact path="/profile" component={Profile} />
           <Footer />
         </Fragment>
-      </Fragment>
+      </Switch>
     </Router>
   </Provider>
 )
